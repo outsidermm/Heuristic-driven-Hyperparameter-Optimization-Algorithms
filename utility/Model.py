03273088da3d87a0eb5785cf6ceea30ad6193d75
outsidermm <71582657+outsidermm@users.noreply.github.com>
2024-07-08 15:59:03 +1000

    move augmentation to model setup

--- utility/Model.py
+++ utility/Model.py
@@ -1,19 +1,6 @@
 from keras import layers, models, regularizers
 
 
-def data_augmentation() -> models.Sequential:
-    preprocessing = models.Sequential()
-    # Preprocessing - Data Augmentation
-    preprocessing.add(layers.RandomContrast(factor=0.2, seed=42))
-    preprocessing.add(layers.RandomFlip(mode="HORIZONTAL", seed=42))
-    preprocessing.add(layers.RandomRotation(factor=0.1, seed=42))
-    preprocessing.add(
-        layers.RandomTranslation(height_factor=0.1, width_factor=0.1, seed=42)
-    )
-
-    return preprocessing
-
-
 def normalisation() -> models.Sequential:
     normalisation = models.Sequential()
     normalisation.add(layers.Rescaling(scale=1.0 / 255))
@@ -40,6 +27,13 @@ def VGG16(input_shape, num_class, weight_decay=0.0005):
 
     model = models.Sequential()
     model.add(layers.Input(shape=input_shape))
+
+    # Preprocessing - Data Augmentation
+    model.add(layers.RandomFlip(mode="HORIZONTAL", seed=42))
+    model.add(layers.RandomRotation(factor=0.1, seed=42))
+    model.add(layers.RandomTranslation(height_factor=0.1, width_factor=0.1, seed=42))
+
+    # Preprocessing - VGG-16
     model.add(
         layers.Conv2D(
             64,
